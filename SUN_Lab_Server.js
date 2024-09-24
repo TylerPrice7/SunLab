1730b759e0ae8e68516d8ee843630eef261f9659
TylerPrice7 <122813643+TylerPrice7@users.noreply.github.com>
2024-09-23 21:46:57 -0400

    New functionality to server.
    Allows the usage of timestamp page and login page.

--- SUN_Lab_Server.js
+++ SUN_Lab_Server.js
@@ -1,7 +1,18 @@
 const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const PORT = 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.json());
+
+// Very secure
+const admin_username = "admin"
+const admin_password = "password123";
+userAuthenticated = false;
+
 
 const db = new sqlite3.Database('./sqlite.db', (err) => {
     if (err) 
@@ -9,19 +20,18 @@ const db = new sqlite3.Database('./sqlite.db', (err) => {
     else { console.log("Connected to the database."); }
 });
 
-app.set('view engine', 'ejs');
-app.use(express.json());
 
 app.listen(PORT, (error) => {
     if(error)
         console.log("Error occurred, server can't start", error);
     else {
-        console.log("Server is Successfully Running, " + "and App is listening on port " + PORT);
+        console.log("Server is Successfully Running" + " on port " + PORT);
     }
 });
 
-// Example endpoint to get access logs
+// Admin front page. Redirects to login if not authenticated.
 app.get('/', (req, res) => {
+    if (userAuthenticated) {
     const { id_filter, date_filter, date_times } = req.query;
     
     let query = `SELECT * FROM RECORDS WHERE true`;
@@ -47,4 +57,39 @@ app.get('/', (req, res) => {
         }
         res.render('index', { records: rows, id_filter: id_filter, date_filter: date_filter, date_times: date_times });
     });
+    }
+    else { res.redirect('login'); }
+});
+
+
+app.get('/timestamp', (req, res) => {
+    res.render('timestamp');
+});
+
+// Inserts id and timestamps into the database.
+app.post('/timestamp', (req, res) => {
+    const user_id = req.body.user_id;
+    const time = new Date();
+    const timestamp = time.toLocaleDateString('en-CA') + ' ' + time.toLocaleTimeString('en-US', {hour12: false});
+    let query = "INSERT or REPLACE INTO RECORDS (user_id, timestamp) VALUES (";
+    query += user_id + ", '" + timestamp + "');";
+    
+    db.run(query);
+
+    res.render('timestamp');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+// Checks if login information is admin.
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username == admin_username && password == admin_password) {
+        userAuthenticated = true;
+        res.redirect('/');
+    }
+    else { res.render('login'); }
 });
